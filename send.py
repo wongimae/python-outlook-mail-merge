d4c6068c8993d8e1bd6437f1a9cb457ce08ae226
sarah wong <sarahwong9566@gmail.com>
2022-11-22 14:35:18 +0800

    updated

--- send.py
+++ send.py
@@ -9,18 +9,22 @@ for index, row in df.iterrows(): #Loops through each row in the dataframe
     email = (row['email'])  #Sets dataframe variable, 'email' to cells in column 'Email Addresss'
     subject = (row['subject']) #Sets dataframe variable, 'subject' to cells in column 'Subject'
     content = str((row['content'])) #Sets dataframe variable, 'body' to cells in column 'Email HTML Body'
-    voucher = str((row['voucher'])) #Sets dataframe variable, 'voucher' to cells in column 'voucher'
+    # voucher = str((row['voucher'])) #Sets dataframe variable, 'voucher' to cells in column 'voucher'
 
-    if (pd.isnull(email) or pd.isnull(subject) or pd.isnull(content) or pd.isnull(voucher)): #Skips over rows where one of the cells in the three main columns is blank
+    if (pd.isnull(email) or pd.isnull(subject) or pd.isnull(content)): #Skips over rows where one of the cells in the three main columns is blank
         continue
 
     olMailItem = 0x0 #Initiates the mail item object
     obj = win32com.client.Dispatch("Outlook.Application") #Initiates the Outlook application
     newMail = obj.CreateItem(olMailItem) #Creates an Outlook mail item
     newMail.Subject = subject #Sets the mail's subject to the 'subject' variable
-    newMail.HTMLbody = (r"" + content +  "" + voucher) #Sets the mail's body to 'body' variable
+    newMail.HTMLbody = "(<HTML>content</HTML>)"
+    # print(content)
+    # newMail.HTMLbody = ("" + content + "\r\n" + voucher) #Sets the mail's body to 'body' variable
+    # print(type(newMail.HTMLbody))
+
 
     newMail.To = email #Sets the mail's To email address to the 'email' variable
     newMail.display() #Displays the mail as a draft email
     
-    newMail.Send()
+    # newMail.Send()
